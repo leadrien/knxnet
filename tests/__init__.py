50c2c3eeedb6da650084f4d2f62ec9ad8137d39f
Adrien Lescourt <adrien.lescourt@gmail.com>
2016-05-24 10:33:06 +0200

    Add shebang, fix setup.py to load README.md

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,8 @@
-__author__ = 'adrien'
+# -*- coding: utf-8 -*-
+
+__author__ = "Adrien Lescourt"
+__copyright__ = "HES-SO 2014, Project EMG4B"
+__credits__ = ["Adrien Lescourt"]
+__version__ = "1.0.0"
+__email__ = "adrien.lescourt@gmail.com"
+__status__ = "Prototype"
